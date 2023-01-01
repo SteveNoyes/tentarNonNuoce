783dddcb8594def7325bf23ad7d1f5c30c5bba60
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-01 13:13:21 -0500

    linked access

--- js/script.js
+++ js/script.js
@@ -3,5 +3,19 @@ var app = {
   timer: document.getElementsByClassName('timer')[0],
   startTime: new Date().getTime(),
   currentTime: new Date().getTime(),
-  interval: undefined
+  interval: undefined,
+
+  startTimer: function(time){
+    app.startTime = Math.ceil(new Date().getTime() + time);
+    clearInterval(app.interval);
+    app.interval = setInterval(function() {
+       app.currentTime = new Date().getTime();
+       app.time = app.convertMillToString(app.startTime - app.currentTime);
+       console.log('Counting down: ' + app.time);
+       app.timer.innerHTML = app.time;
+       
+       if(app.time == '0:0')
+          clearInterval(app.interval);
+    }, 500);
+ }
 }
